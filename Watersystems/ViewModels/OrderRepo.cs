edfa2ad4a7639b59d7e48a755ee164adcafdad25
Michael Søndergaard <m.soendergaard87@gmail.com>
2021-11-24 13:24:21 +0100

    Repos added

---
+++ Watersystems/ViewModels/OrderRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watersystems.Models;
+
+namespace Watersystems.ViewModels
+{
+    public class OrderRepo
+    {
+        private List<Order> orders = new List<Order>();
+
+        private string dataFileName = "Order.csv";
+
+        public void Create(int orderNumber, string orderedBy)
+        {
+            orders.Add(new Order(orderNumber, orderedBy));
+        }
+        public void Create(int orderNumber, string orderedBy, string recivedBy)
+        {
+            orders.Add(new Order(orderNumber, orderedBy, recivedBy));
+        }
+        public Order Get(int orderNumber)
+        {
+            Order result = null;
+            foreach (Order order in orders)
+            {
+                if (orderNumber == order.OrderNumber)
+                {
+                    result = order;
+                    break;
+                }
+            }
+            return result;
+        }
+        public List<Order> GetAll()
+        {
+            return orders;
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete(int orderNumber)
+        {
+            Order order = this.Get(orderNumber);
+            if (order != null)
+            {
+                orders.Remove(order);
+            }
+        }
+    }
+}
---
+++ Watersystems/ViewModels/ProductRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watersystems.Models;
+
+namespace Watersystems.ViewModels
+{
+    public class ProductRepo
+    {
+        private List<Product> products = new List<Product>();
+
+        private string dataFileName = "Product.csv";
+
+        public void Create(string productName, int productNumber, double quantity, string unitType)
+        {
+            products.Add(new Product(productName, productNumber, quantity, unitType));
+        }
+        public Product Get(int productNumber)
+        {
+            Product result = null;
+            foreach (Product product in products)
+            {
+                if (productNumber == product.ProductNumber)
+                {
+                    result = product;
+                    break;
+                }
+            }
+            return result;
+        }
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete(int productNumber)
+        {
+            Product product = this.Get(productNumber);
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+
+        }
+    }
+}
